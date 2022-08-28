292f3a9764b5729bf27ef89e877949155ee04aa7
Eran Zimbler <blog@darklines.quest>
2022-08-28 19:58:25 +0300

    update validator to force placeholder

--- backend/app/models/domain/items.py
+++ backend/app/models/domain/items.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 from app.models.common import DateTimeModelMixin, IDModelMixin
 from app.models.domain.profiles import Profile
 from app.models.domain.rwmodel import RWModel
-
+from pydantic import validator
 
 class Item(IDModelMixin, DateTimeModelMixin, RWModel):
     slug: str
@@ -15,3 +15,10 @@ class Item(IDModelMixin, DateTimeModelMixin, RWModel):
     favorites_count: int
     image: Optional[str]
     body: Optional[str]
+
+    class Config:
+        validate_assignment = True
+
+    @validator('image')
+    def set_image(cls, image):
+        return image or '/placeholder.png'
